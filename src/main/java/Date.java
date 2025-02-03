7cf698a24b2af292e7e0de49fa6c7d389980a577
Thomas Pendleton III <105595323+Tpenny68@users.noreply.github.com>
2025-02-03 13:48:49 -0500

    Another submission 2.0

--- src/main/java/Date.java
+++ src/main/java/Date.java
@@ -132,36 +132,37 @@ public class Date {
      * @return the number of days in the month given by the parameter month
      */
     public static int getDaysInMonth(int month, int year){
-        if (month == 1){
-            return 31;
-        } else if (month == 2){
-            if (year % 4 == 0){
-                return 29;
-            } else {
-                return 28;
-            }
-        } else if (month == 3){
-            return 31;
-        } else if (month == 4){
-            return 30;
-        } else if (month == 5){
-            return 31;
-        } else if (month == 6){
-            return 30;
-        } else if (month == 7){
-            return 31;
-        } else if (month == 8) {
-            return 31;
-        } else if (month == 9){
-            return 30;
-        } else if (month == 10){
-            return 31;
-        } else if (month == 11){
-            return 30;
-        } else if (month == 12){
-            return 31;
-        } else {
-            return -1;
+        switch (month){
+            case 1:
+                return 31;
+                case 2:
+                    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                        return 29;
+                    } else {
+                        return 28;
+                    }
+                    case 3:
+                        return 31;
+                        case 4:
+                            return 30;
+                            case 5:
+                                return 31;
+                                case 6:
+                                    return 30;
+                                    case 7:
+                                        return 31;
+                                        case 8:
+                                            return 31;
+                                            case 9:
+                                                return 30;
+                                                case 10:
+                                                    return 31;
+                                                    case 11:
+                                                        return 30;
+                                                        case 12:
+                                                            return 31;
+                                                            default:
+                                                                return -1;
         }
     }
 
@@ -234,27 +235,6 @@ public class Date {
      * @return the day of the week that the date falls on
      */
     public static int dayOfWeek(int month, int dayOfMonth, int year){
-        /**
-        System.out.println("The month is " + getMonthName(month));
-        System.out.println("The months number is " + month);
-        System.out.println("The day of the month is " + dayOfMonth);
-        int startingDay = ((year + ((year-1) / 4) - ((year-1) / 100) + ((year-1) / 400)) % 7);
-        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            daysInMonth[1] = 29;
-        }
-        int totalDays = 0;
-        if (month <= 12){
-            for (int i = 0; i < month - 1; i++) {
-                totalDays += daysInMonth[i];
-            }
-            totalDays += dayOfMonth;
-            return (startingDay + totalDays - 1) % 7;
-        } else {
-            return -1;
-        }
-         */
-
         int startingDay = ((year + ((year-1) / 4) - ((year-1) / 100) + ((year-1) / 400)) % 7);
         int monthDays = 0;
         int theTotalDays = 0;
@@ -265,71 +245,59 @@ public class Date {
         } else {
             leapDays = 28;
         }
-        if (month == 1){
-            System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-            return (startingDay) % 7;
-        } else if (month == 2){
-            if (dayOfMonth <= leapDays){
-                monthDays = 31;
-                theTotalDays = monthDays + dayOfMonth;
-                System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-                return (startingDay + theTotalDays -1) % 7;
-            } else {
-                return -1;
-            }
-        } else if (month == 3){
-            monthDays = 31;
-            theTotalDays = leapDays + monthDays + dayOfMonth;
-            System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-            return (startingDay + theTotalDays -1) % 7;
-        } else if (month == 4){
-            monthDays = 31*(2);
-            theTotalDays = monthDays + leapDays + dayOfMonth;
-            System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-            return (startingDay + theTotalDays -1) % 7;
-        } else if (month == 5){
-            monthDays = 31*(2) + 30;
-            theTotalDays = monthDays + leapDays + dayOfMonth;
-            System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-            return (startingDay + theTotalDays -1) % 7;
-        } else if (month == 6){
-            monthDays = 31*(3) + 30;
-            theTotalDays = monthDays + leapDays + dayOfMonth;
-            System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-            return (startingDay + theTotalDays -1) % 7;
-        } else if (month == 7){
-            monthDays = 31*(3) + 30*(2);
-            theTotalDays = monthDays + leapDays + dayOfMonth;
-            System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-            return (startingDay + theTotalDays -1) % 7;
-        } else if (month == 8){
-            monthDays = 31*(4) + 30*(2);
-            theTotalDays = monthDays + leapDays + dayOfMonth;
-            System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-            return (startingDay + theTotalDays -1) % 7;
-        } else if (month == 9){
-            monthDays = 31*(5) + 30*(2);
-            theTotalDays = monthDays + leapDays + dayOfMonth;
-            System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-            return (startingDay + theTotalDays -1) % 7;
-        } else if (month == 10){
-            monthDays = 31*(5) + 30*(3);
-            theTotalDays = monthDays + leapDays + dayOfMonth;
-            System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-            return (startingDay + theTotalDays -1) % 7;
-        } else if (month == 11){
-            monthDays = 31*(6) + 30*(3);
-            theTotalDays = monthDays + leapDays + dayOfMonth;
-            System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-            return (startingDay + theTotalDays -1) % 7;
-        } else if (month == 12){
-            monthDays = 31*(6) + 30*(4);
-            theTotalDays = monthDays + leapDays + dayOfMonth;
-            System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-            return (startingDay + theTotalDays -1) % 7;
-        } else {
-            System.out.println("Month: " + month + " Year: " + year + " Day: " + dayOfMonth + "The day of the week is " + ((startingDay + theTotalDays -1) % 7));
-            return (startingDay + theTotalDays -1) % 7;
+        switch (month){
+            case 1:
+                return (startingDay) % 7;
+                case 2:
+                    if (dayOfMonth <= leapDays){
+                        monthDays = 31;
+                        theTotalDays = monthDays + dayOfMonth;
+                        return (startingDay + theTotalDays -1) % 7;
+                    } else {
+                        return -1;
+                    }
+                    case 3:
+                        monthDays = 31;
+                        theTotalDays = leapDays + monthDays + dayOfMonth;
+                        return (startingDay + theTotalDays -1) % 7;
+                        case 4:
+                            monthDays = 31*(2);
+                            theTotalDays = leapDays + monthDays + dayOfMonth;
+                            return (startingDay + theTotalDays -1) % 7;
+                            case 5:
+                                monthDays = 31*(2) + 30;
+                                theTotalDays = leapDays + monthDays + dayOfMonth;
+                                return (startingDay + theTotalDays -1) % 7;
+                                case 6:
+                                    monthDays = 31*(3) + 30;
+                                    theTotalDays = leapDays + monthDays + dayOfMonth;
+                                    return (startingDay + theTotalDays -1) % 7;
+                                    case 7:
+                                        monthDays = 31*(3) + 30*(2);
+                                        theTotalDays = leapDays + monthDays + dayOfMonth;
+                                        return (startingDay + theTotalDays -1) % 7;
+                                        case 8:
+                                            monthDays = 31*(4) + 30*(2);
+                                            theTotalDays = leapDays + monthDays + dayOfMonth;
+                                            return (startingDay + theTotalDays -1) % 7;
+                                            case 9:
+                                                monthDays = 31*(5) + 30*(2) + 30;
+                                                theTotalDays = leapDays + monthDays + dayOfMonth;
+                                                return (startingDay + theTotalDays -1) % 7;
+                                                case 10:
+                                                    monthDays = 31*(5) + 30*(3);
+                                                    theTotalDays = leapDays + monthDays + dayOfMonth;
+                                                    return (startingDay + theTotalDays -1) % 7;
+                                                    case 11:
+                                                        monthDays = 31*(6) + 30*(3);
+                                                        theTotalDays = leapDays + monthDays + dayOfMonth;
+                                                        return (startingDay + theTotalDays -1) % 7;
+                                                        case 12:
+                                                            monthDays = 31*(6) + 30*(4);
+                                                            theTotalDays = leapDays + monthDays + dayOfMonth;
+                                                            return (startingDay + theTotalDays -1) % 7;
+                                                            default:
+                                                                return -1;
         }
     }
 
@@ -341,46 +309,70 @@ public class Date {
      * @return the day of the week that the date falls on
      */
     public static int dayOfWeek(String month, int dayOfMonth, int year){
-        System.out.println(month);
-        int monthNumber = getMonthNumber(month);
-        int monthInt = 0;
-        if (month == "January"){
-            monthInt = 1;
-        } else if (month == "February"){
-            monthInt = 2;
-        } else if (month == "March"){
-            monthInt = 3;
-        } else if (month == "April"){
-            monthInt = 4;
-        } else if (month == "May"){
-            monthInt = 5;
-        } else if (month == "June"){
-            monthInt = 6;
-        } else if (month == "July"){
-            monthInt = 7;
-        } else if (month == "August"){
-            monthInt = 8;
-        } else if (month == "September"){
-            monthInt = 9;
-        } else if (month == "October"){
-            monthInt = 10;
-        } else if (month == "November"){
-            monthInt = 11;
-        } else if (month == "December"){
-            monthInt = 12;
-        } else {
-            return -1;
-        }
         int startingDay = ((year + ((year-1) / 4) - ((year-1) / 100) + ((year-1) / 400)) % 7);
-        int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            daysInMonth[1] = 29;
+        int monthNumber = getMonthNumber(month);
+        int monthDays = 0;
+        int theTotalDays = 0;
+        int leapDays = 0;
+        System.out.println(month);
+        if (isLeapYear(year)){
+            leapDays = 29;
+        } else {
+            leapDays = 28;
         }
-        int totalDays = 0;
-        for (int i = 0; i < monthInt - 1; i++) {
-            totalDays += daysInMonth[i];
+        switch (monthNumber){
+            case 1:
+                return (startingDay) % 7;
+            case 2:
+                if (dayOfMonth <= leapDays){
+                    monthDays = 31;
+                    theTotalDays = monthDays + dayOfMonth;
+                    return (startingDay + theTotalDays -1) % 7;
+                } else {
+                    return -1;
+                }
+            case 3:
+                monthDays = 31;
+                theTotalDays = leapDays + monthDays + dayOfMonth;
+                return (startingDay + theTotalDays -1) % 7;
+            case 4:
+                monthDays = 31*(2);
+                theTotalDays = leapDays + monthDays + dayOfMonth;
+                return (startingDay + theTotalDays -1) % 7;
+            case 5:
+                monthDays = 31*(2) + 30;
+                theTotalDays = leapDays + monthDays + dayOfMonth;
+                return (startingDay + theTotalDays -1) % 7;
+            case 6:
+                monthDays = 31*(3) + 30;
+                theTotalDays = leapDays + monthDays + dayOfMonth;
+                return (startingDay + theTotalDays -1) % 7;
+            case 7:
+                monthDays = 31*(3) + 30*(2);
+                theTotalDays = leapDays + monthDays + dayOfMonth;
+                return (startingDay + theTotalDays -1) % 7;
+            case 8:
+                monthDays = 31*(4) + 30*(2);
+                theTotalDays = leapDays + monthDays + dayOfMonth;
+                return (startingDay + theTotalDays -1) % 7;
+            case 9:
+                monthDays = 31*(5) + 30*(2) + 30;
+                theTotalDays = leapDays + monthDays + dayOfMonth;
+                return (startingDay + theTotalDays -1) % 7;
+            case 10:
+                monthDays = 31*(5) + 30*(3);
+                theTotalDays = leapDays + monthDays + dayOfMonth;
+                return (startingDay + theTotalDays -1) % 7;
+            case 11:
+                monthDays = 31*(6) + 30*(3);
+                theTotalDays = leapDays + monthDays + dayOfMonth;
+                return (startingDay + theTotalDays -1) % 7;
+            case 12:
+                monthDays = 31*(6) + 30*(4);
+                theTotalDays = leapDays + monthDays + dayOfMonth;
+                return (startingDay + theTotalDays -1) % 7;
+            default:
+                return -1;
         }
-        totalDays += dayOfMonth;
-        return (startingDay + totalDays - 1) % 7;
     }
 }
